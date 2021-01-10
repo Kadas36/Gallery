2a7d16835ef74159fb73284d2612ff90e2ce1fb6
kadas36 <oduorthomas14@gmail.com>
2021-01-10 12:35:03 +0300

    Add tests

--- gallery/models.py
+++ gallery/models.py
@@ -10,7 +10,11 @@ class location(models.Model):
         return self.name
 
     class Meta:
-        ordering = ['name']    
+        ordering = ['name'] 
+
+    def save_location(self):
+        self.save()
+       
 
 class Category(models.Model):
     name = models.CharField(max_length =30)
@@ -21,6 +25,10 @@ class Category(models.Model):
     class Meta:
         ordering = ['name'] 
 
+    def save_category(self):
+        self.save()
+    
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'gallery/')
     name = models.CharField(max_length=30)
@@ -28,10 +36,37 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     location = models.ForeignKey('location', on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
-    date_posted = models.DateTimeField(auto_now=True) 
+    date_posted = models.DateTimeField(auto_now_add=True) 
 
     class Meta:
         ordering = ['date_posted']   
 
     def __str__(self):
-        return self.name                       
+        return self.name  
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id, target, update):
+        image_updated = cls.objects.filter(id=id).update(target=update)
+        return image_updated
+
+  
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        results = cls.objects.filter(category__icontains=search_term)
+        return results
+
+    @classmethod
+    def filter_by_location(cls, search_term):
+        results = cls.objects.filter(location__icontains=search_term)
+        return results                         
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,69 @@
 from django.test import TestCase
+from .models import Category,location,Image
+import datetime as dt
 
 # Create your tests here.
+
+
+#location class method tests
+class locationTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.kenya= location(name = 'Kenya')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.kenya,location))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.kenya.save_location()
+        locations = location.objects.all()
+        self.assertTrue(len(locations) > 0)  
+
+#Category class method tests
+class CategoryTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.nature= Category(name = 'Nature')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nature,Category))
+
+    # Testing Save Method
+    def test_save_category(self):
+        self.nature.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)  
+
+#Image class method tests     
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+
+        # Creating a new location and saving it
+        self.kenya= location(name="Kenya")
+        self.kenya.save_location()
+
+        # Creating a new category and saving it
+        self.nature= Category(name="Nature")
+        self.nature.save_category()
+
+
+        self.img1= Image(   image="testImage",
+                            name="img1",
+                            url="Testurl",
+                            description="This is a test",
+                            location=self.kenya
+                        )
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.img1,Image))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.img1.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)   
