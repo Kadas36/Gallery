0ccf37811f4bf8c87f803b73e13bfa94edc3a73f
kadas36 <oduorthomas14@gmail.com>
2021-01-10 11:14:47 +0300

    Update gallery view

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import location,Category,Image
 
 # Register your models here.
+
+class GalleryAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
+
+admin.site.register(location)
+admin.site.register(Category)
+admin.site.register(Image)
