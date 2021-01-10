7ce8ddbab63a3078e1c6b738498d58a6a657e7aa
kadas36 <oduorthomas14@gmail.com>
2021-01-10 11:13:50 +0300

    Update Category to Image relation

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 
@@ -18,4 +19,19 @@ class Category(models.Model):
         return self.name   
 
     class Meta:
-        ordering = ['name']          
+        ordering = ['name'] 
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'gallery/')
+    name = models.CharField(max_length=30)
+    url = models.TextField()
+    description = models.TextField(blank=True)
+    location = models.ForeignKey('location', on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    date_posted = models.DateTimeField(auto_now=True) 
+
+    class Meta:
+        ordering = ['date_posted']   
+
+    def __str__(self):
+        return self.name                       
