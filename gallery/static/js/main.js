19587231baf689885d118dcdef037d4bc2280892
kadas36 <oduorthomas14@gmail.com>
2021-01-11 02:19:34 +0300

    Add location routing

--- gallery/static/js/main.js
+++ gallery/static/js/main.js
@@ -1,3 +1,4 @@
+// modal
 var modal = document.getElementById("imageModal");
 var i;
 for (i = 0; i < 20; i++) {
@@ -6,15 +7,13 @@ for (i = 0; i < 20; i++) {
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
     var imageUrl = document.getElementById('url')
-    console.log(imageUrl)
     img.onclick = function(){
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
         imageUrl.innerHTML = this.src;
-    
 
-    
+        
     var span = document.getElementsByClassName("close")[0];
     span.onclick = function() { 
         modal.style.display = "none";
@@ -22,6 +21,17 @@ for (i = 0; i < 20; i++) {
 }
     
 }
+
+// copy url function
+function copyFunction() {
+    var copyUrl = document.getElementById("url").innerHTML;
+    console.log(copyUrl);
+    // copyUrl.select();
+    document.execCommand("Copy");
+    alert("Copied the text: " + copyUrl);
+}    
+
+
 
 
 
--- gallery/views.py
+++ gallery/views.py
@@ -3,8 +3,16 @@ from .models import location,Category,Image
 
 # Create your views here.
 
-def landing(request):
-    return render(request, 'landing.html')
+def location_page(request):
+    if 'image' in request.GET and request.GET["image"]:
+        filter_location = request.GET.get("image")
+        image_results = Image.filter_by_location(filter_location)
+        
+        return render(request, 'gallerylab/base.html',{"images": image_results})
+
+    else:
+        image_results = Image.filter_by_location('Kenya')
+        return render(request, 'gallerylab/base.html',{"images": image_results})
 
 def gallery_page(request):
     if 'image' in request.GET and request.GET["image"]:
