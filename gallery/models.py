8c658dcf1c63bbe993f6d599d8d462aa76e1d1eb
kadas36 <oduorthomas14@gmail.com>
2021-01-11 13:19:54 +0300

    Update image model

--- gallery/models.py
+++ gallery/models.py
@@ -55,10 +55,8 @@ class Image(models.Model):
     description = models.TextField(blank=True)
     location = models.ForeignKey('location', on_delete=models.CASCADE)
     category = models.ManyToManyField(Category)
-    date_posted = models.DateTimeField(auto_now_add=True) 
 
-    class Meta:
-        ordering = ['date_posted']   
+     
 
     def __str__(self):
         return self.name  
